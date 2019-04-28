5419e0f051d276c42321a98c3e8e228f54965fd2
Lukasz <szloser.lukasz@gmail.com>
2019-04-28 23:15:04 +0200

    Cleanup

--- db/migrate/20190420140410_create_dogs.rb
+++ db/migrate/20190420140410_create_dogs.rb
@@ -3,9 +3,11 @@ class CreateDogs < ActiveRecord::Migration[5.1]
     create_table :dogs do |t|
       t.timestamps
       t.string :url
-      t.string :picture_id, unique: true
+      t.string :picture_id
       t.string :secret, unique: true
       t.integer :cuteness, default: 0
     end
+
+    add_index :dogs, :picture_id, unique: true
   end
 end
