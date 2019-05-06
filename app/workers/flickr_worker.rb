98d0f85f84f2d9a8dbddc61d85992b478c865a21
Lukasz <szloser.lukasz@gmail.com>
2019-05-06 23:36:57 +0200

    Worker changes

--- app/workers/flickr_worker.rb
+++ app/workers/flickr_worker.rb
@@ -2,13 +2,21 @@ require 'flickr'
 
 class FlickrWorker
   include Sidekiq::Worker
+
   def perform
+    saveCount = 0
+    page = 1
     flickr = Flickr.new
-    list = flickr.photos.search(content_type: 1, safe_search: 1, tags: [:dog, :cute, :animal], tag_mode: :all, per_page: 300, page: 1)
-    list.reject { |dog| Dog.all.pluck(:picture_id).include? dog.id}.first(100).each do |obj|
-      url = Flickr.url obj
-      dog = Dog.new(url: url, picture_id: obj.id, secret: obj.secret, cuteness: 0)
-      dog.save
+    while saveCount < 100 do
+      list = flickr.photos.search(content_type: 1, safe_search: 1, tags: [:dog, :cute, :animal], tag_mode: :all, per_page: 100 - saveCount, page: page)
+      list.reject { |dog| Dog.all.pluck(:picture_id).include? dog.id}.each do |obj|
+        url = Flickr.url obj
+        dog = Dog.new(url: url, picture_id: obj.id, secret: obj.secret, cuteness: 0)
+        if dog.save
+          saveCount += 1
+        end
+      end
+      page += 1
     end
   end
 end
